6f08717d9fdf4a32541657d6e4d21f5303bf46c5
Ashish kumar <ashishk6621@gmail.com>
2026-04-27 00:39:43 +0530

    ProductRepository test

--- src/test/java/com/ashish/elasticsearch_project/AbstractTest.java
+++ src/test/java/com/ashish/elasticsearch_project/AbstractTest.java
@@ -1,6 +1,10 @@
 package com.ashish.elasticsearch_project;
 
+import com.ashish.elasticsearch_project.sec03.QueryMethodsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -8,9 +12,14 @@ import org.springframework.core.io.ResourceLoader;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.type.TypeReference;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.function.Consumer;
+
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AbstractTest {
+
+    private static final Logger log = LoggerFactory.getLogger(QueryMethodsTest.class);
 
 	@Autowired
     private ObjectMapper mapper;
@@ -18,7 +27,7 @@ public class AbstractTest {
     @Autowired
     private ResourceLoader loader;
 
-    protected<T> T readSource(String path, TypeReference<T> typeReference){
+    protected<T> T readResource(String path, TypeReference<T> typeReference){
         try{
             var classpath = "classpath" + path;
             var file = this.loader.getResource(classpath).getFile();
@@ -27,6 +36,10 @@ public class AbstractTest {
         catch (Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    protected<T> Consumer<T> print(){
+        return t -> log.info("{}",t);
     }
 
 }
--- src/test/java/com/ashish/elasticsearch_project/sec03/QueryMethodsTest.java
+++ src/test/java/com/ashish/elasticsearch_project/sec03/QueryMethodsTest.java
@@ -1,10 +1,17 @@
 package com.ashish.elasticsearch_project.sec03;
 
 import com.ashish.elasticsearch_project.AbstractTest;
+import com.ashish.elasticsearch_project.sec03.entity.Product;
 import com.ashish.elasticsearch_project.sec03.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.type.TypeReference;
+
+import java.util.List;
 
 public class QueryMethodsTest extends AbstractTest {
 
@@ -13,5 +20,26 @@ public class QueryMethodsTest extends AbstractTest {
     @Autowired
     private ProductRepository repository;
 
+    @BeforeAll
+    public void dataSetup(){
+        var products = this.readResource("sec03/products.json", new TypeReference<List<Product>>() {
+        });
+        this.repository.saveAll(products);
+        Assertions.assertEquals(20,this.repository.count());
+    }
+
+    @Test
+    public void findByCategory(){
+        var searchHits = this.repository.findByCategory("furniture");
+        searchHits.stream().forEach(this.print());
+        Assertions.assertEquals(4,searchHits.getTotalHits());
+    }
+
+    @Test
+    public void findByCategories(){
+        var searchHits = this.repository.findByCategoryIn(List.of("furniture","Beauty"));
+        searchHits.stream().forEach(this.print());
+        Assertions.assertEquals(4,searchHits.getTotalHits());
+    }
 
 }
